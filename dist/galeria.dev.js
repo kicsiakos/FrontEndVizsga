661f6b7f3dd741bfdcef7adf80840abab96afdb9
kicsiakos <148480849+kicsiakos@users.noreply.github.com>
2024-01-08 10:17:40 +0100

    01.08.

--- dist/galeria.dev.js
+++ dist/galeria.dev.js
@@ -135,3 +135,13 @@ function rahuz17() {
 function lehuz17() {
   document.getElementById("kep17-szoveg").style.display = "none";
 }
+
+function rahuz() {
+  var x = document.getElementsByClassName("nav-horgonyok")[0];
+
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+}
--- galeria.js
+++ galeria.js
@@ -133,3 +133,12 @@ function rahuz17() {
 function lehuz17() {
     document.getElementById("kep17-szoveg").style.display = "none"
 }
+
+function rahuz() {
+    var x = document.getElementsByClassName("nav-horgonyok")[0];
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
