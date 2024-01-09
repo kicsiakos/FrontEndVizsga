93c9b36eba345b90ecd235f7a4d1cea1848b68e8
kicsiakos <148480849+kicsiakos@users.noreply.github.com>
2024-01-09 14:33:26 +0100

    01.09.v3

--- idopontfoglalas.js
+++ idopontfoglalas.js
@@ -1,3 +1,12 @@
 function uzenet() {
     alert("Köszönjük foglalását, kollégánk hamarosan felveszi Önnel a kapcsolatot!")
 }
+
+function rahuz() {
+    var x = document.getElementsByClassName("nav-horgonyok")[0];
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
