f2809ba30e7a840497d75b7556cfaa8f708f629c
kicsiakos <148480849+kicsiakos@users.noreply.github.com>
2024-04-24 13:37:47 +0200

    kiterjesztések módosítása

--- arlistaMiklos.php
+++ arlistaMiklos.php
@@ -26,27 +26,27 @@
                         <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
                     </svg></div>
                 <ul class="nav-horgonyok">
-                    <li><a href="index.html">Bemutatkozás</a></li>
-                    <li class="dropdown"><a href="valaszto2.html" class="dropdown-button">Sminktetoválás <svg
+                    <li><a href="index.php">Bemutatkozás</a></li>
+                    <li class="dropdown"><a href="valaszto2.php" class="dropdown-button">Sminktetoválás <svg
                                 xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
                                 <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
                             </svg></a>
                         <div class="dropdown-content">
-                            <a href="interlash.html">Szempillatetoválás</a>
-                            <a href="aquarellelips.html">Ajaktetoválás</a>
-                            <a href="softliner.html">Szemhéjtetoválás</a>
+                            <a href="interlash.php">Szempillatetoválás</a>
+                            <a href="aquarellelips.php">Ajaktetoválás</a>
+                            <a href="softliner.php">Szemhéjtetoválás</a>
                         </div>
                     </li>
-                    <li class="dropdown"><a href="russianlips.html" class="dropdown-button">Esztétika <svg
+                    <li class="dropdown"><a href="russianlips.php" class="dropdown-button">Esztétika <svg
                                 xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
                                 <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
                             </svg></a>
                         <div class="dropdown-content">
-                            <a href="russianlips.html">Ajakfeltöltés</a>
+                            <a href="russianlips.php">Ajakfeltöltés</a>
                         </div>
                     </li>
-                    <li><a href="galeria.html">Galéria</a></li>
-                    <li class="dropdown"><a href="valaszto.html" class="dropdown-button">Árlista <svg
+                    <li><a href="galeria.php">Galéria</a></li>
+                    <li class="dropdown"><a href="valaszto.php" class="dropdown-button">Árlista <svg
                                 xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
                                 <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
                             </svg></a>
