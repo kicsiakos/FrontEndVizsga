e30d1da3984fde68e98741403eb74493eb682a89
kicsiakos <148480849+kicsiakos@users.noreply.github.com>
2024-04-24 13:23:35 +0200

    logolások elkészítése

--- loginproc.php
+++ loginproc.php
@@ -13,33 +13,44 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
     $conn = new mysqli($servername, $user, $pass, $database);
     if (!$conn) {
+        $errorMessage = date("Y.m.d H:i:s") . " Nem sikerült kapcsolódni az adatbázishoz!\n";
+        $myFile = fopen("errorLog.txt", "a") or die("Nem lehet megnyitni a file-t!");
+        fwrite($myFile, $errorMessage);
+        fclose($myFile);
         header('Location: login.php');
-        //logba beírom a hibát
     }
-    $sql = "SELECT id, email, jelszo FROM felhasznalok WHERE email = '" . $username ."'";
+    $sql = "SELECT id, email, jelszo FROM felhasznalok WHERE email = '" . $username . "'";
     $result = $conn->query($sql);
     if ($result) {
         if ($result->num_rows === 1) {
             $row = $result->fetch_assoc();
-            if (password_verify($password.$salt, $row['jelszo'])) {
+            if (password_verify($password . $salt, $row['jelszo'])) {
 
                 $_SESSION['id'] = $row['id'];
                 $_SESSION['logindate'] = date('YYYY.MM.DD');
                 header('Location: index.php');
 
             } else {
-                //logolni a sikertelen probalkozást email cím jó, jelszó nem jó
-                $_SESSION['error'] = $login_error;
+                $errorMessage = date("Y.m.d H:i:s") . " E-mail cím jó de a jelszó helytelen\n";
+                $myFile = fopen("errorLog.txt", "a") or die("Nem lehet megnyitni a file-t!");
+                fwrite($myFile, $errorMessage);
+                fclose($myFile);
+                $_SESSION['error'] = "Helytelen jelszó!";
                 header('Location: login.php');
             }
         } else {
-            //logolni a sikertelen probalkozást email cím nem volt jó
-            //IP cím érdemes tárolni
-            //$_SERVER['REMOTE_ADDR'];
-            $_SESSION['error'] = $login_error;
+            $errorMessage = date("Y.m.d H:i:s") . " E-mail cím helytelen ($username) " . "IP: " . $_SERVER['REMOTE_ADDR'] . "\n";
+            $myFile = fopen("errorLog.txt", "a") or die("Nem lehet megnyitni a file-t!");
+            fwrite($myFile, $errorMessage);
+            fclose($myFile);
+            $_SESSION['error'] = "Helytelen e-mail cím!";
             header('Location: login.php');
         }
     } else {
+        $errorMessage = date("Y.m.d H:i:s") . " Adatbázis hiba!\n";
+        $myFile = fopen("errorLog.txt", "a") or die("Nem lehet megnyitni a file-t!");
+        fwrite($myFile, $errorMessage);
+        fclose($myFile);
         header('Location: login.php');
     }
     $conn->close();
